8ce4d40bf9439d3bbdc967d3e462227a4241d524
Eka Risyana <eka.risyana@mekari.com>
2022-11-29 07:24:51 +0700

    simple todo with typescript implemented in data()

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,67 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+<script lang="ts">
+import { defineComponent } from "vue";
+
+declare interface BaseComponentData {
+  todo: string;
+  todos: string[];
+}
+
+export default defineComponent({
+  data(): BaseComponentData {
+    return {
+      todo: "",
+      todos: [],
+    };
+  },
+
+  methods: {
+    addTodo(): void {
+      this.todos.push(this.todo);
+      this.todo = 1;
+    },
+    deleteTodo(todo: string): void {
+      this.todos = this.todos.filter((data: string) => data !== todo);
+    },
+  },
+});
 </script>
 
 <template>
   <div id="app">
     <header>
-      <img
-        alt="Vue logo"
-        class="logo"
-        src="@/assets/logo.svg"
-        width="125"
-        height="125"
-      />
-
       <div class="wrapper">
-        <HelloWorld msg="You did it!" />
+        <h1>TODO</h1>
 
-        <nav>
-          <router-link to="/">Home</router-link>
-          <router-link to="/about">About</router-link>
-        </nav>
+        <form action="" @submit.prevent="addTodo">
+          <input v-model="todo" type="text" name="todo" id="todo" />
+          <button type="submit">add todo</button>
+        </form>
+
+        <div class="todo-list">
+          <hr />
+          <div class="list-item" v-for="(todo, index) in todos" :key="index">
+            <span>{{ todo }}</span>
+            <button @click="deleteTodo(todo)">x</button>
+          </div>
+        </div>
       </div>
     </header>
-
-    <router-view />
   </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+input {
+  margin-right: 8px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+hr {
+  margin: 10px 0;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.todo-list {
+  width: 230px;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.list-item {
+  display: flex;
+  margin: 3px 0;
+  justify-content: space-between;
 }
 </style>
